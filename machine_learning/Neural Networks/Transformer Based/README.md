88276886c9a085c5397988976aefc31bf65df21c
nishan <nishan@pop-os.localdomain>
2024-02-14 18:16:37 -0800

    LLAMA i madeit work somehoe

--- machine_learning/Neural Networks/Transformer Based/README.md
+++ machine_learning/Neural Networks/Transformer Based/README.md
@@ -3,7 +3,8 @@
 - https://builtin.com/artificial-intelligence/transformer-neural-network
 - Notes were taken mostly from video into a paper on how transformer works [Google Drive Link](https://drive.google.com/drive/folders/1oQyxuUMdzIhkFmApY-ocnekL-_GMjHXm?usp=sharing)
 - langchain reference using open source model Good Learning Resource in [Github](https://github.com/krishnaik06/The-Grand-Complete-Data-Science-Materials).
-- [Video tutorial for using langchain](https://youtu.be/cMJWC-csdK4?si=ZZ7vAw--Ds_CZ-3S) 
+- [Video tutorial for using langchain](https://youtu.be/cMJWC-csdK4?si=ZZ7vAw--Ds_CZ-3S)
+- [Langchain tutorial](https://youtu.be/2xxziIWmaSA?si=05_MX2w9nJqbfI8B)
 - https://huggingface.co/meta-llama/Llama-2-7b
 - Integrate open source LLM using langhcain and [CTransformers](https://python.langchain.com/docs/integrations/providers/ctransformers) 
 - https://www.youtube.com/watch?v=cEyHsMzbZBs
--- machine_learning/Neural Networks/Transformer Based/nishan.py
+++ machine_learning/Neural Networks/Transformer Based/nishan.py
@@ -1,9 +1,26 @@
-from langchain_community.llms import CTransformers
 
-llm=CTransformers(model="./llama-2-7b-chat.ggmlv3.q8_0.bin",
-                   model_type="llama",config={
-                       "max_new_tokens":256,
-                       "temperature":"0.01"
-                   })
-response=llm("Hello")
-print(response)
+from langchain.prompts import Prompt
+from langchain.llms import CTransformers
+
+## Function To get response from LLAma 2 model
+def getLLamaresponse():
+
+    ### LLama2 model
+    llm=CTransformers(model='./llama-2-7b-chat.ggmlv3.q8_0.bin',
+                      model_type='llama',
+                      config={'max_new_tokens':800,
+                              'temperature':0.01})
+    
+    ## Prompt Template
+
+    template="""Tell me what SQL vulnerability with a common example"""
+    
+    #prompt= Prompt(template)
+    
+    ## Generate the ressponse from the LLama 2 model
+    response=llm(template)
+    print(response)
+    print(len(response))
+    return response
+
+getLLamaresponse()
