671a83bea651636ff2b090d21b9d80a0ca836e57
nishan <nishan@pop-os.localdomain>
2024-02-17 17:09:12 -0800

    information about RAG application

---
+++ machine_learning/Neural Networks/Projects/Use pdfs embedding knowledge/README.md
@@ -0,0 +1,17 @@
+# RAG
+- I did not know this was called Retreival Augmented Generateion (RAG).
+```
+One of the most powerful applications enabled by LLMs is sophisticated question-answering (Q\&A) chatbots. These are applications that can answer questions about specific source information. These applications use a technique known as Retrieval Augmented Generation, or RAG.
+
+```
+- Idea is `use LLM like metas llama, and make it answers questions via a embeddings generated form a document.`
+- Reference:
+    - This github [code](https://github.com/AIAnytime/Llama2-Medical-Chatbot/blob/main/model.py) is helpful.
+    - Youtube [video](https://youtu.be/kXuHxI5ZcG0?si=4euSa1K9dd8ZL6-b).
+    - About embeddings [article](https://datasciencedojo.com/blog/embeddings-and-llm/#).
+- About RAG application [information](https://python.langchain.com/docs/use_cases/question_answering/). This is such a good article. I love it.
+- RAG architecture high-level overview:
+**Indexing**: a pipeline for ingesting data from a source and indexing it. *This usually happens offline.*
+
+**Retrieval and generation**: the actual RAG chain, which takes the user query at run time and retrieves the relevant data from the index, then passes that to the model.
+- Looki into the [link](https://python.langchain.com/docs/use_cases/question_answering/) for more information.
--- machine_learning/Neural Networks/Transformer Based/n.py
+++ machine_learning/Neural Networks/Transformer Based/n.py
@@ -22,4 +22,57 @@ def create_vector_db():
     db = FAISS.from_documents(texts,embeddings)
     db.save_local(output)
 
-create_vector_db()
+
+def create_prompt():
+    custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+context: {context}
+Question: {question}
+Only return the helpful answer and nothing else.
+Helpful answer:
+"""
+    prompt=PromptTemplate(template=custom_prompt_template,
+                          input_variables=['context','question'])
+    return prompt
+
+def create_qa_chain(llm,prompt,db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
+
+def load_module():
+
+    # loading the metas llama LLM
+    print("Loading LLM")
+    llm = CTransformers(
+        model="llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        max_new_token=512,
+        temperature=0.5,
+    )
+    print("LLM Loaded")
+    print("Loading embeddings")
+    embeddings=HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={'device':'cpu'}
+    )
+    db = FAISS.load_local(output,embeddings)
+    print("embeddings DB loaded")
+
+    qa=create_qa_chain(llm,create_prompt(),db)
+    print("custom chain created")
+    return qa
+
+def main():
+    llm = load_module()
+    print(llm({'query':"what is malware analysis?"}))
+    print(llm({'query':"How to treat tounge infection disease?"}))
+    print(llm({'query':"Why are stars so bright?"}))
+   #create_vector_db()
+
+main()
